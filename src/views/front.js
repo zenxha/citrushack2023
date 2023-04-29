9e49435a8069aeb50386e91bf74cd9b3a0adfd46
Dongyeon Kim <dongyeonkim@MacBook-Pro-230.local>
2023-04-29 10:33:28 -0700

    submission form base

--- src/views/front.js
+++ src/views/front.js
@@ -1,10 +1,10 @@
 const fill = document.querySelector('.fill');
-const empty = document.querySelector('.empty');
+const empty = document.querySelector('.submitform');
 
 //Fill listeners
 fill.addEventListener('dragstart', dragStart);
 
 //functions
 function dragStart(evt) {
-    
+
 }
